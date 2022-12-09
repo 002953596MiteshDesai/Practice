dcb5a3378b87f775e1d9a8e36f52ee5839c9d2b6
Mitesh <miteshdesai888@gmail.com>
2022-12-09 14:02:44 -0800

    Adding MinimumRemoveParenthesis

---
+++ src/MinimumRemoveParenthesis.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class MinimumRemoveParenthesis {
+	
+	private static String returnValidString(String str){
+		Stack<Integer> stack = new Stack<>();
+		ArrayList<Integer> indexes  = new ArrayList<>();
+		for(int i = 0 ; i < str.length(); i ++) {
+			if(str.charAt(i) == '('){
+				stack.push(i);
+			}else if (str.charAt(i) == ')'){
+				if(stack.isEmpty()){
+					indexes.add(i);
+				}else{
+					stack.pop();
+				}
+			}
+		}
+		while(!stack.isEmpty()){
+			indexes.add(stack.pop());
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0 ; i < str.length(); i ++){
+			if(!indexes.contains(i)){
+				sb.append(str.charAt(i));
+			}
+		}
+		
+		return sb.toString();
+	}
+}
+
